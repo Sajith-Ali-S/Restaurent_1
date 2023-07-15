c39ec95fcb28231132c006748e68af87947b4aec
Sajith Ali S <119950449+Sajith-Ali-S@users.noreply.github.com>
2023-07-16 00:19:41 +0530

    Add files via upload
    Initial Commit,  works needs to be done yet

---
+++ script.js
@@ -0,0 +1,11 @@
+// fetch("https://raw.githubusercontent.com/saksham-accio/f2_contest_3/main/food.json")
+// .then(res=>res.json())
+// .then(data=>console.log(data))
+// .catch(err=>console.log(err))
+
+
+const container = document.getElementsByClassName("container");
+function renderHomepage(){
+container.innerHTML = ``;
+console.log("HomePage clicked");
+}
